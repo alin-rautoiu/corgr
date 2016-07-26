f5278336f7d06907a7b44a95cd6a04535ccd9e7b
alin.rautoiu <alin.rautoiu@teamnet.ro>
2016-07-26 09:08:09 +0300

    Add id to dogs, extract function to make dogs, add more items in likes list

--- corgr.js
+++ corgr.js
@@ -2,50 +2,59 @@ var dogDir = "dogs";
 var basePictureUrl = 'https://raw.githubusercontent.com/alin-rautoiu/corgr/master';
 var dogs = [
     {
+        "id" : "0",
         "face" : "resized-0.jpg",
         "name" : "Mr. Fluffles",
-        "likes" : ["Cuddles"]
+        "likes" : ["Cuddles", "Chasing for pokemons"]
     },
     {
+        "id" : "1",
         "face" : "resized-1.jpg",
         "name" : "Sir McDrummies",
-        "likes" : ["Noms"]
+        "likes" : ["Noms", "Squeaky toys"]
     },
-        {
+    {
+        "id" : "2",
         "face" : "resized-2.jpg",
         "name" : "Princess Butter",
-        "likes" : ["Kisses"]
+        "likes" : ["Kisses", "Noms"]
     },
-        {
+    {
+        "id" : "3",
         "face" : "resized-3.jpg",
         "name" : "Duke Squiggy Derpenshire",
         "likes" : ["Rubs on the belly"]
     },
-        {
+    {
+        "id" : "4",
         "face" : "resized-4.jpg",
         "name" : "Doctor Bubbles",
         "likes" : ["When mommy and daddy aren't fighting"]
     },
-        {
+    {
+        "id" : "4",
         "face" : "resized-5.jpg",
         "name" : "Duchess Olivia",
         "likes" : ["Walkies"]
     },
-        {
+    {
+        "id" : "5",
         "face" : "resized-6.jpg",
         "name" : "Lady Bunnytail",
         "likes" : ["To run around sheep"]
     },
-        {
+    {
+        "id" : "6",
         "face" : "resized-7.jpg",
         "name" : "Miss Pickles",
         "likes" : ["To eat mommy's food when she's pretending not to look"]
     },
-        {
+    {
+        "id" : "7",
         "face" : "resized-8.jpg",
         "name" : "Loki",
         "likes" : ["To pretend he's a real dog."]
-    },
+    }
 ]
 
 var express = require('express');
@@ -56,16 +65,20 @@ var app = express();
 
 app.engine('html', require('ejs').renderFile);
 
+function makeDogDto (element) {
+    return {
+               "id" : element.id,
+               "pathToFace" : basePictureUrl + path.sep + dogDir + path.sep + element.face, 
+               "name" : element.name, 
+               "likes" : element.likes
+            }
+}
+
 function makeDogsDto() {
     var allDogs = [];
 
     dogs.forEach(function(element) {
-       allDogs.push(
-           {
-               "pathToFace": basePictureUrl + path.sep + dogDir + path.sep + element.face, 
-               "name" : element.name, 
-               "likes" : element.likes
-            }); 
+       allDogs.push(makeDogDto(element)); 
     }, this);
     return allDogs;
 }
@@ -82,11 +95,7 @@ app.get('/getCorgi/:id', function(req, res) {
         return;
     }
 
-    var dog = {
-        "pathToFace": basePictureUrl + path.sep + dogDir + path.sep + dogs[id].face, 
-        "name" : dogs[id].name, 
-        "likes": dogs[id].likes
-    };
+    var dog = makeDogDto(dogs[id]);
 
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(dog));
@@ -122,11 +131,7 @@ app.get('/getRandomCorgi/', function(req, res) {
         return;
     }
 
-    var dog = {
-        "pathToFace": basePictureUrl + path.sep + dogDir + path.sep + dogs[id].face, 
-        "name" : dogs[id].name, 
-        "likes": dogs[id].likes
-    };
+    var dog = makeDogDto(dogs[id]);
 
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(dog));
